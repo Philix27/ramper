1b0078faa1ff2938a7eade38013b8bd4029c4246
Philix27 <philixbob@gmail.com>
2024-07-10 09:22:36 +0100

    mini: added account page UI, display of email and phone

--- apps/mini/app/(core)/account/page.tsx
+++ apps/mini/app/(core)/account/page.tsx
@@ -1,14 +1,41 @@
 import React from "react";
 import Navbar from "../_comps/navbar";
 import { AiFillEdit } from "react-icons/ai";
+import { TextH, TextP } from "@repo/ui";
 
 export default function AccountPage() {
   return (
     <>
       <Navbar title={"Profile"} icon={AiFillEdit} />
       <div className={"flex flex-col items-center px-8 py-4 mb-10"}>
-        <div>AccountPage</div>
+        <div className="w-full my-4">
+          <TextH v="h5" className="text-white">
+            Personal
+          </TextH>
+          <RowItem left={"Email"} right={"myemail@gmail.com"} />
+          <RowItem left={"Phone"} right={"2348108850572"} />
+        </div>
+        <div className="w-full mb-4">
+          <TextH v="h5" className="text-white">
+            Beneficiary
+          </TextH>
+          <RowItem left={"Phone"} right={"2348108850572"} />
+          <RowItem left={"Phone"} right={"2348108850572"} />
+          <RowItem left={"Phone"} right={"2348108850572"} />
+          <RowItem left={"Phone"} right={"2348108850572"} />
+          <RowItem left={"Phone"} right={"2348108850572"} />
+          <RowItem left={"Phone"} right={"2348108850572"} />
+        </div>
       </div>
     </>
+  );
+}
+
+function RowItem(props: { left: string; right: string }) {
+  return (
+    <div className="flex justify-between items-center py-2 border-b border-accent">
+      <TextP className="text-white">{props.left} </TextP>
+      <TextP>{props.right}</TextP>
+    </div>
   );
 }
---
+++ apps/mini/app/(core)/gift/context.ts
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+type IContext = {}
+
+const GiftContext = createContext("");
