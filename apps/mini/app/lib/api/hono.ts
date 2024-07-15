4264e2cde0314aaf6c6552eac34071c138273258
Philix27 <philixbob@gmail.com>
2024-07-15 19:28:03 +0100

    comment out apiClient path

--- apps/mini/app/lib/api/hono.ts
+++ apps/mini/app/lib/api/hono.ts
@@ -1,10 +1,10 @@
 import { hc } from "hono/client";
 
-import { type AppType } from "../../../../server"
+// import { type AppType } from "../../../../server"
 
-const client = hc<AppType>(process.env.NEXT_PUBLIC_SERVER_URL!)
+// const client = hc<AppType>(process.env.NEXT_PUBLIC_SERVER_URL!)
 
-// type AppType = any;
-// const client: any = hc<AppType>(process.env.NEXT_PUBLIC_SERVER_URL!);
+type AppType = any;
+const client: any = hc<AppType>(process.env.NEXT_PUBLIC_SERVER_URL!);
 
 export const ApiClient = client.api;
