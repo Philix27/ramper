adfb3acf785658523267933e70caccc08aa1004d
Philix27 <philixbob@gmail.com>
2024-07-15 19:17:03 +0100

    mini: added write for createCard

--- apps/mini/app/(core)/gift/create.tsx
+++ apps/mini/app/(core)/gift/create.tsx
@@ -6,8 +6,9 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { transferCusdTokens } from "@/contract/const";
+import { AppContract, transferCusdTokens } from "@/contract/const";
 import { useMinipay } from "@/contract";
+import { useWriteContract } from "wagmi";
 
 export const formSchema = z.object({
   purpose: z.string().max(30, { message: "Maximum of 25 characters" }),
@@ -15,8 +16,8 @@ export const formSchema = z.object({
   card_owner: z.string().optional(),
   phone: z
     .string()
-    .max(10, { message: "Maximum of 10 numbers" })
-    .min(10, { message: "Minimum of 10 numbers" })
+    .max(11, { message: "Maximum of 11 numbers" })
+    .min(11, { message: "Minimum of 11 numbers" })
     .optional(),
 });
 
@@ -34,7 +35,7 @@ type IAmount = 100 | 200 | 300 | 400 | 500 | 1000;
 export default function CreateGiftCard() {
   const [sendTo, setSendTo] = useState<ISendTo>("PHONE");
   const [amount, setAmount] = useState<IAmount>(100);
-
+  const { writeContract } = useWriteContract();
   const { walletAddress } = useMinipay();
 
   const form = useForm<IFormSchema>({
@@ -74,9 +75,15 @@ export default function CreateGiftCard() {
     console.log("Called mutate", parseInt(amount as unknown as string) / 1500);
     const trxnHash = await transferCusdTokens({
       userAddress: walletAddress!,
-      amount: parseInt(amount as unknown as string) / 1500,
+      amount: parseInt(amount as unknown as string),
     });
     console.log("Hashed", trxnHash);
+    writeContract({
+      abi: AppContract.abi,
+      address: AppContract.address as `0x${string}`,
+      functionName: "createCard",
+      args: [value.phone, parseInt(amount as unknown as string),],
+    });
     // mutation.mutate({
     //   purpose: value.purpose,
     //   phone: value.phone,
@@ -84,6 +91,7 @@ export default function CreateGiftCard() {
     //   card_owner: value.card_owner,
     // });
   }
+
   return (
     <div className="w-full mt-4">
       <TextP className="mb-2">Send to:</TextP>
@@ -141,12 +149,11 @@ export default function CreateGiftCard() {
             setAmount(e.target.value as unknown as IAmount);
           }}
         >
-          <option value={100}>₦100</option>
-          <option value={200}>₦200</option>
-          <option value={300}>₦300</option>
-          <option value={400}>₦400</option>
-          <option value={500}>₦500</option>
-          <option value={600}>₦1,000</option>
+          <option value={1}>$1</option>
+          <option value={2}>$2</option>
+          <option value={3}>$3</option>
+          <option value={4}>$4</option>
+          <option value={5}>$5</option>
         </select>
       </div>
 
