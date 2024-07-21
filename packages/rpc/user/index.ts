b5574df19d80e5c12f2101dbf11f93c8dc1c3168
Philix27 <philixbob@gmail.com>
2024-07-21 08:15:28 +0100

    rpc: updated user types

--- packages/rpc/user/index.ts
+++ packages/rpc/user/index.ts
@@ -16,27 +16,52 @@ const schema = {
     first_name: z.string(),
     last_name: z.string(),
   }),
+  update: z.object({
+    themeIsDark: z.boolean(),
+  }),
 };
 
 export const UserSchema = schema;
 //* Return Schema
-export type IUserCreate = {
-  phone: string;
-  first_name: string;
-};
 
+export const ReturnType = {
+  update: z.object({
+    msg: z.string(),
+  }),
+  create: z.object({
+    phone: z.string(),
+    firstName: z.string(),
+  }),
+  kyc: z.object({
+    msg: z.string(),
+  }),
+};
 //* Interface for Service and RPC
 export interface IUser {
-  create(props: z.infer<typeof schema.create>): Promise<IUserCreate>;
-  kyc(props: z.infer<typeof schema.kyc>): Promise<IUserCreate>;
+  create(
+    props: z.infer<typeof schema.create>
+  ): Promise<z.infer<typeof ReturnType.create>>;
+  kyc(
+    props: z.infer<typeof schema.kyc>
+  ): Promise<z.infer<typeof ReturnType.kyc>>;
 }
 
 //* RPC Schema
 export class UserRpc implements IUser {
-  async kyc(props: z.infer<typeof schema.kyc>): Promise<IUserCreate> {
+  async kyc(
+    props: z.infer<typeof schema.kyc>
+  ): Promise<z.infer<typeof ReturnType.kyc>> {
     return await request(props);
   }
-  async create(props: z.infer<typeof schema.create>): Promise<IUserCreate> {
+  async create(
+    props: z.infer<typeof schema.create>
+  ): Promise<z.infer<typeof ReturnType.create>> {
+    return await request(props);
+  }
+
+  async update(
+    props: z.infer<typeof schema.create>
+  ): Promise<z.infer<typeof ReturnType.update>> {
     return await request(props);
   }
 }
